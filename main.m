1b1f67a1378fec192970b73bd6bea753b6ee995a
skilkis <sankilkis@msn.com>
2018-10-01 21:43:23 +0200

    Added main file to demonstrate some functionality. Eventually this will trigger the optimization to run

---
+++ main.m
@@ -0,0 +1,11 @@
+%% Demonstrates simple yet powerful directory management w/ packages
+clc
+clear all
+close all
+
+obj = geometry.CSTAirfoil(linspace(0, 1, 100)');
+
+% To see the power of classes try accessing the plot method by
+% typing obj.plot() after running. These are dynamic calls to the object
+% and can proove indispensible when asking for data or storing history
+% of optimizations
