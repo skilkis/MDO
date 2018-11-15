f59f58da54d01071caea1220299b0af0492c3158
Evert Bunschoten <evertbun@xs4all.nl>
2018-11-15 23:52:18 +0100

    Updated loads and added wing weight and W_aw to template

--- +loads/Loads.m
+++ +loads/Loads.m
@@ -1,6 +1,6 @@
 classdef Loads
     properties
-        %Root chord Bernstein coefficients
+        
 
         W_f;         %Design fuel weight[kg]
         W_w;         %Wing weight[kg]
@@ -16,27 +16,29 @@ classdef Loads
         Y_coord;
         L_distr;
         M_distr;
-        A_r;% = ones(1,12);
-        A_t;% = ones(1,12);
+        A_r;
+        A_t;
         b;
         p;
 
-    h = 11248;          % Cruise altitude[m]
-    
-    M_c;
-    rho;        % Cruise altitude air density[kg m^-3]
+        h;          % Cruise altitude[m]
+        a_c;
+        M_mo;
+        
+        rho;        % Cruise altitude air density[kg m^-3]
 
-    g;           % Acceleration due to gravity[ms^-2]
-    n;        % Maximum load factor
-    v;      % Viscosity of air at 215 K[m^2s^-1]
-    W_aw;       % Aircraft less wing weight[kg]
-    
+        g;           % Acceleration due to gravity[ms^-2]
+        n;        % Maximum load factor
+        v;      % Viscosity of air at 215 K[m^2s^-1]
+        W_aw;       % Aircraft less wing weight[kg]
+        W_pl_des;    % Design payload weight [kg]
+        n_max;
         
     end
    methods
    function obj = Loads(aircraft_in)
-            obj.W_f = aircraft_in.W_f_hat;
-            obj.W_w = aircraft_in.W_w_hat;
+            obj.W_f = aircraft_in.W_f;
+            obj.W_w = aircraft_in.W_w;
             obj.A_r = aircraft_in.A_root.';
             obj.A_t = aircraft_in.A_tip.';
             obj.Chords = aircraft_in.Chords;
@@ -48,10 +50,11 @@ classdef Loads
             obj.p = aircraft_in.planform;
             
             obj.h = aircraft_in.h_c;
-            obj.M_c = aircraft_in.M_c;
+            obj.a_c = aircraft_in.a_c;
+            obj.M_mo = aircraft_in.M_mo;
             obj.rho = aircraft_in.rho_c;
             obj.g = aircraft_in.g;
-            obj.n_max = aircraft_in.n_max;
+            obj.n_max = aircraft_in.eta_max;
             obj.v = aircraft_in.v;
             obj.W_aw = aircraft_in.W_aw;
             
@@ -59,32 +62,29 @@ classdef Loads
    end 
    
    function AC = get.AC(obj)
-        P = obj.p;
-        MTOW = obj.W_aw + obj.W_f + obj.W_w;
-        W_des = sqrt(MTOW*(MTOW - obj.W_f));
-        T_c = 288.15 - 0.0065*obj.h;
-
-        V_c = obj.M_c*sqrt(1.4*287*T_c/(1+0.2*obj.M_c^2));
-
-        C_L = obj.n_max*obj.g*W_des/(0.5*obj.rho*obj.S*V_c^2);
+        
+        MTOW = obj.W_aw + obj.W_pl_des + obj.W_f + obj.W_w;
+        
+        V_c = obj.M_mo*obj.a_c;
+        
+        C_L = obj.n_max*obj.g*MTOW/(0.5*obj.rho*obj.S*V_c^2);
         AC.Wing.Geom = [obj.Coords, obj.Chords.', obj.Twists.'];
         AC.Wing.inc = 0;
-        %AC.Wing.eta = [0;2*P.gamma/P.b;1];
+        
         AC.Wing.eta = [0;1];
         AC.Visc = 0;
-        %AC.Wing.Airfoils = [obj.A_r;obj.A_r - (obj.A_r-obj.A_t)*2*P.gamma/P.b;obj.A_t];
         AC.Wing.Airfoils = [obj.A_r;obj.A_t];
         AC.Aero.rho = obj.rho;
         AC.Aero.alt = obj.h;
-        AC.Aero.M = obj.M_c;%obj.V_c/sqrt(1.4*287*215.038);
+        AC.Aero.M = obj.M_mo;
         AC.Aero.Re = V_c*obj.MAC/obj.v;
 
-        AC.Aero.V = obj.M_c*sqrt(1.4*287*T_c/(1+0.2*obj.M_c^2));
+        AC.Aero.V = obj.M_mo*obj.a_c;
         AC.Aero.CL = C_L;
    end
     
    function res = fetch_Res(obj)
-   res = aerodynamics.Q3D_solver(obj.AC);
+        res = aerodynamics.Q3D_solver(obj.AC);
    end
     
    function Y = get.Y_coord(obj)
@@ -95,9 +95,8 @@ classdef Loads
     function L = get.L_distr(obj)
         
         Y = obj.Res.Wing.Yst;
-        T_c = 288.15 - 0.0065*obj.h;
 
-        V_c = obj.M_c*sqrt(1.4*287*T_c/(1+0.2*obj.M_c^2));
+        V_c = obj.M_mo*obj.a_c;
         q = 0.5*obj.rho*obj.S*V_c^2;
         Ccl = obj.Res.Wing.ccl;
         
@@ -107,16 +106,16 @@ classdef Loads
         L = [Ccl0,Ccl.',Ccl1]*q;
     end
     function M = get.M_distr(obj)
-        T_c = 288.15 - 0.0065*obj.h;
-
-        V_c = obj.M_c*sqrt(1.4*287*T_c/(1+0.2*obj.M_c^2));
+        V_c = obj.M_mo*obj.a_c;
+        
         q = 0.5*obj.rho*obj.S*V_c^2;
         Y = obj.Res.Wing.Yst;
         Cm = obj.Res.Wing.cm_c4;
         ends = interp1(Y,Cm, [0; 0.5*obj.b],'pchip','extrap');
         Cm0 = ends(1);
         Cm1 = ends(2);
-        M = [Cm0,Cm.',Cm1].*q.*[obj.Chords(1), obj.Res.Wing.chord.',obj.Chords(3)].*obj.MAC;
+        M = [Cm0,Cm.',Cm1].*q.*[obj.Chords(1), obj.Res.Wing.chord.',...
+            obj.Chords(3)].*obj.MAC;
     end
    end
    
